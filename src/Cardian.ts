6108c3af0cf4ef11da89a41e6c8ebf719deba4c2
TheVeiler <duboisvalentin1010@gmail.com>
2026-03-28 17:51:17 +0100

    Factorized components

--- src/Cardian.ts
+++ src/Cardian.ts
@@ -3,24 +3,27 @@ import * as decklists from "./decklists/index";
 // import * as blackjack from "./blackjack/index";
 // import * as holdem from "./holdem/index";
 
-type Card = typeof common.Card; //| typeof blackjack.Card | holdem.Card;
-type Box = typeof common.Box; //| typeof blackjack.Box | holdem.Box;
-type List = typeof common.List; //| typeof blackjack.List | holdem.List;
+interface IComponents {
+	Card: Card;
+	Box: Box;
+	List: List;
+}
+type Card = typeof common.Card; // | typeof blackjack.Card; // | typeof holdem.Card;
+type Box = typeof common.Box; // | typeof blackjack.Box; // | typeof holdem.Card;
+type List = typeof common.List; // | typeof blackjack.List; // | typeof holdem.Card;
 
-type Mode = "none" | "blackjack";
+type Mode = "none"; // | "blackjack";
 
 class Cardian {
-	#Card: Card;
+	#components: IComponents;
 	get Card() {
-		return this.#Card;
+		return this.#components.Card;
 	}
-	#Box: Box;
 	get Box() {
-		return this.#Box;
+		return this.#components.Box;
 	}
-	#List: List;
 	get List() {
-		return this.#List;
+		return this.#components.List;
 	}
 
 	#standard52: common.PseudoCard[];
@@ -33,9 +36,8 @@ class Cardian {
 	}
 
 	constructor() {
-		this.#Card = common.Card;
-		this.#Box = common.Box;
-		this.#List = common.List;
+		this.#components = common;
+		console.log(this.#components);
 
 		this.#standard52 = decklists.standard52;
 		this.#standard52_sextet = decklists.standard52_sextet;
@@ -44,21 +46,15 @@ class Cardian {
 	setMode(mode: Mode = "none"): void {
 		switch (mode) {
 			case "none":
-				this.#Card = common.Card;
-				this.#Box = common.Box;
-				this.#List = common.List;
+				this.#components = common;
 				break;
 
 			// case "blackjack":
-			// 	this.#Card = blackjack.Card;
-			// 	this.#Box = blackjack.Box;
-			// 	this.#List = blackjack.List;
+			// 	this.#components = blackjack;
 			// 	break;
 
 			// case "holdem":
-			// 	this.#Card = holdem.Card;
-			// 	this.#Box = holdem.Box;
-			// 	this.#List = holdem.List;
+			//     this.#components = holdem;
 			// 	break;
 		}
 	}
